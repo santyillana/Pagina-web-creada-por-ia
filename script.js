93696ca48ed5d9adbec22257dd70823260a6b9df
santyillana <dsantiagosilvah@gmail.com>
2025-04-30 23:53:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 fetch('productos.json')
-    .then(res => res.json())
-    .then(data => {
-        const container = document.getElementById('productos-lista');
-        data.forEach(producto => {
-            const div = document.createElement('div');
-            div.className = 'producto';
-            div.innerHTML = `
-                <img src="images/${producto.imagen}" alt="${producto.nombre}">
-                <h3>${producto.nombre}</h3>
-                <p>${producto.descripcion}</p>
-            `;
-            container.appendChild(div);
-        });
-    })
-    .catch(error => {
-        console.error("Error cargando los productos:", error);
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('productos-lista');
+    data.forEach(producto => {
+      const div = document.createElement('div');
+      div.className = 'producto';
+      div.innerHTML = `
+        <img src="images/${producto.imagen}" alt="${producto.nombre}" />
+        <h3>${producto.nombre}</h3>
+        <p>${producto.descripcion}</p>
+      `;
+      container.appendChild(div);
     });
+  })
+  .catch(err => {
+    console.error('Error al cargar productos:', err);
+  });
